d171b89ee4c196781e5c6c582dfbff107f2e9aab
Mehmet Emin Alptekin <142679066+Alptekin0@users.noreply.github.com>
2025-03-29 23:59:14 +0300

    multiplication table
    This code generates a multiplication table from 1 to 10.

---
+++ JavaScript/carpimTablosu.js
@@ -0,0 +1,8 @@
+//! ÇARPIM TABLOSU 
+for (let i = 1; i <= 10; i++) {
+     for (let j = 1; j <= 10; j++) {
+          let carpim = i * j;
+          console.log(i + " x " + j + " = " + carpim);
+     }
+     console.log("-------------------");
+}
